3c15ad2a9ef8f82f98dceabd78b3400f8f0d6a09
Vasyl Puziichuk <ipzk221_pvv@student.ztu.edu.ua>
2022-11-13 00:46:16 +0200

    First commit for new branch

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp!");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp!");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp!");
+Console.WriteLine("Рядок тексту1");
+Console.WriteLine("Рядок тексту2");
+Console.WriteLine("Рядок тексту3");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp!");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp!");
